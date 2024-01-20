7fa29f0bbce4486bafc2cb845e96f162da35975d
dhruv <dhruv20345@gmail.com>
2024-01-20 14:26:30 +0530

    flask api created

--- data_extracter.py
+++ data_extracter.py
@@ -10,6 +10,6 @@ def extract_data_from_response(response):
         # Access specific metadata fields
         source = metadata.get('source', 'N/A')
         page_number = metadata.get('page', 'N/A')
-        src_pg_nms.append("Source:"+source+", Page Number:"+str(page_number))
+        src_pg_nms.append("- Source:"+source+", Page Number:"+str(page_number))
     result=response["result"]
     return result,src_data,src_pg_nms
