640ec522bf3eb8c5f52f84c40d0ede6b580a96f9
dhruv <dhruv20345@gmail.com>
2024-03-17 21:19:49 +0530

    vercel

--- main.py
+++ main.py
@@ -84,13 +84,13 @@ async def createTmpChain(background_tasks: BackgroundTasks, files: List[UploadFi
     except Exception as e:
         return JSONResponse(content={"success": False, "error": str(e)})
 
-app = CORSMiddleware(
-    app=app,
-    allow_origins=origins,  # You can replace '*' with specific origins
-    allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],  # or specific methods
-    allow_headers=["Authorization", "Content-Type", "Accept"]  # or specific headers
-)
+# app = CORSMiddleware(
+#     app=app,
+#     allow_origins=origins,  # You can replace '*' with specific origins
+#     allow_credentials=True,
+#     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],  # or specific methods
+#     allow_headers=["Authorization", "Content-Type", "Accept"]  # or specific headers
+# )
 # import uvicorn
 # if __name__ == '__main__':
 #     uvicorn.run(app, host="0.0.0.0", port=8000)
